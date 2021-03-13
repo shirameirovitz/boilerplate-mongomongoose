996f7eb3a4899a0354e94780a954cb19a7c50dd8
Shira-Meirovitz <shirameirovi@gmail.com>
2021-03-13 14:17:14 +0200

    find edit then save

--- myApp.js
+++ myApp.js
@@ -44,11 +44,18 @@ const createManyPeople = (arrayOfPeople, done) => {
   };
   
 
-const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
-};
+  const findEditThenSave = (personId, done) => {
+    const foodToAdd = 'hamburger';
+  
+    Person.findById(personId, (err, person) => {
+      if(err) return console.log(err); 
+      person.favoriteFoods.push(foodToAdd);
+      person.save((err, updatedPerson) => {
+        if(err) return console.log(err);
+        done(null, updatedPerson)
+      })
+    })
+  };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
