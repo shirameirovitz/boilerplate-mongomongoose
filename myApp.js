21c36a138ab6612b1ac5774ce200cd03670b0b3d
Shira-Meirovitz <shirameirovi@gmail.com>
2021-03-13 14:10:16 +0200

    created the personScheme

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
+require('dotenv').config();
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema= new mongoose.Schema({
+name: {
+  type: String,
+  require: true,
+},
+age: Number,
+favoriteFoods: [String],
+});
+let Person= mongoose.model("Person", personSchema);
 
-let Person;
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
